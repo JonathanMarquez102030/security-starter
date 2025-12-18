eef5385eecb6f683a0741cb1be1091543b891c19
Jonathan Marquez <jmp19731@gmail.com>
2025-12-18 11:26:49 -0600

    feat: implementación de actualización de contraseña

--- src/main/java/com/jonathanmarquez/security/security/controller/PasswordController.java
+++ src/main/java/com/jonathanmarquez/security/security/controller/PasswordController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 /**
  * Controlador REST para gestionar el proceso de recuperación de contraseña olvidada.
  *

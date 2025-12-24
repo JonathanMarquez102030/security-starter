1f1cd6abe8296c3433a5a1eefcf4d1d364da89fc
Jonathan Marquez <jmp19731@gmail.com>
2025-12-24 13:13:44 -0600

    refactor: agregar mensajes personalizados a las validaciones en `ResetPasswordRequestDto`

--- src/main/java/com/jonathanmarquez/security/security/model/dto/ResetPasswordRequestDto.java
+++ src/main/java/com/jonathanmarquez/security/security/model/dto/ResetPasswordRequestDto.java
@@ -4,7 +4,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ResetPasswordRequestDto(
-    @NotBlank @Size(min = 8, max = 200) String newPassword,
-    @NotBlank @Size(min = 8, max = 200) String confirmPassword
+    @NotBlank(message = "La contraseña es requerida")
+    @Size(min = 8, max = 200) String newPassword,
+
+    @NotBlank(message = "La confirmación de la contraseña es requerida")
+    @Size(min = 8, max = 200) String confirmPassword
 ) {
 }

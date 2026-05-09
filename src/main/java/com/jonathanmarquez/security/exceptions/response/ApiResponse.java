f0261b115583a4ab3e1b4e67c4a266957d1ac029
Jonathan Marquez <jmp19731@gmail.com>
2026-05-09 12:12:57 -0600

    chore: agregar encabezado de licencia a todos los archivos

--- src/main/java/com/jonathanmarquez/security/exceptions/response/ApiResponse.java
+++ src/main/java/com/jonathanmarquez/security/exceptions/response/ApiResponse.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2026 Jonathan Márquez Pérez.
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
 package com.jonathanmarquez.security.exceptions.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;

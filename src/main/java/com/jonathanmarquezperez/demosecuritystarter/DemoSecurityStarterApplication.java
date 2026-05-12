64dbaa3e9cf96e5044f488501a8360533ec8ffdf
Jonathan Marquez <jonathan.marquez@thtec.mx>
2026-05-12 16:46:10 -0600

    minimum-config

---
+++ src/main/java/com/jonathanmarquezperez/demosecuritystarter/DemoSecurityStarterApplication.java
@@ -0,0 +1,13 @@
+package com.jonathanmarquezperez.demosecuritystarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityStarterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoSecurityStarterApplication.class, args);
+  }
+
+}
